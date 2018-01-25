1c3ada19b18daa0d614c1825abe0ebad6217553b
Jakub Kordek <jakub@droplr.com>
2018-01-25 19:37:22 +0100

    add more selectable btn and fix styles

--- src/components/common/menu/MenuList.jsx
+++ src/components/common/menu/MenuList.jsx
@@ -26,11 +26,11 @@ class MenuList extends React.Component {
       <div className="menuList">
         <MenuItem doOnClick={this.onClick.bind(this)} label="Aktualności" selected={this.isSelected('Aktualności')}/>
         <MenuItem doOnClick={this.onClick.bind(this)} label="Dane o placówkach" selected={this.isSelected('Dane o placówkach')}/>
-        <MenuItem doOnClick={this.onClick.bind(this)} label="Dokumenty" />
-        <MenuItem doOnClick={this.onClick.bind(this)} label="Forum" />
-        <MenuItem doOnClick={this.onClick.bind(this)} label="Kalendarz" />
-        <MenuItem doOnClick={this.onClick.bind(this)} label="Finanse" />
-        <MenuItem doOnClick={this.onClick.bind(this)} label="Raporty" />
+        <MenuItem doOnClick={this.onClick.bind(this)} label="Dokumenty" selected={this.isSelected('Dokumenty')}/>
+        <MenuItem doOnClick={this.onClick.bind(this)} label="Forum" selected={this.isSelected('Forum')}/>
+        <MenuItem doOnClick={this.onClick.bind(this)} label="Kalendarz" selected={this.isSelected('Kalendarz')}/>
+        <MenuItem doOnClick={this.onClick.bind(this)} label="Finanse" selected={this.isSelected('Finanse')}/>
+        <MenuItem doOnClick={this.onClick.bind(this)} label="Raporty" selected={this.isSelected('Raporty')}/>
       </div>
     )
   }
