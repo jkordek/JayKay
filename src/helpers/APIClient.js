06e70af3874e842fd7905ca4ebd778e8e4acf138
Necromos <necromos@necroworks.net>
2018-04-25 15:27:07 +0200

    Enhanced APIClient with Mesages and comments

--- src/helpers/APIClient.js
+++ src/helpers/APIClient.js
@@ -13,6 +13,68 @@ class APIClient {
     this.config.auth = { username, password };
   }
 
+  getUsers(author) {
+    const params = { author };
+    return this._get('/users', { params });
+  }
+
+  getUser(id) {
+    return this._get(`/users/${id}`);
+  }
+
+  createUser(title, author, content) {
+    const data = { title, author, content };
+    return this._post('/users', data);
+  }
+
+  updateUser(id, title, author, content) {
+    const data = { title, author, content };
+    return this._put(`/users/${id}`, data);
+  }
+
+  deleteUser(id) {
+    return this._delete(`/users/${id}`);
+  }
+
+  /* MESSAGES
+    title   : required : String
+    author  : required : ID
+    place   : optional : ID
+    user    : optional : ID
+    content : required : String
+  */
+
+  getMessages(place, user) {
+    const params = { place, user };
+    return this._get('/messages', { params });
+  }
+
+  getMessage(id) {
+    return this._get(`/messages/${id}`);
+  }
+
+  createMessage(title, author, place, user, content) {
+    const data = {
+      title, author, place, user, content,
+    };
+    return this._post('/messages', data);
+  }
+
+  updateMessage(id, title, author, place, user, content) {
+    const data = {
+      title, author, place, user, content,
+    };
+    return this._put(`/messages/${id}`, data);
+  }
+
+  deleteMessage(id) {
+    return this._delete(`/messages/${id}`);
+  }
+
+  /* NEWS
+    author  : optional : String
+  */
+
   getNews(author) {
     const params = { author };
     return this._get('/news', { params });
@@ -21,6 +83,12 @@ class APIClient {
   getOneNews(id) {
     return this._get(`/news/${id}`);
   }
+
+  /* NEWS
+    title   : required : String
+    author  : required : String
+    content : required : String
+  */
 
   createNews(title, author, content) {
     const data = { title, author, content };
@@ -36,6 +104,12 @@ class APIClient {
     return this._delete(`/news/${id}`);
   }
 
+
+  /* PLACES
+    manager        : optional : Id
+    salesman       : optional : Id
+    administrator  : optional : Id
+  */
   getPlaces(manager, salesman, administrator) {
     const params = {
       manager, salesman, administrator,
@@ -46,6 +120,17 @@ class APIClient {
   getPlace(id) {
     return this._get(`/places/${id}`);
   }
+
+  /* PLACES
+    name           : required : String
+    street         : required : String
+    buildingNumber : required : String
+    postCode       : required : String length 5
+    city           : required : String
+    manager        : optional : Id
+    salesman       : optional : Id
+    administrator  : optional : Id
+  */
 
   createPlace(name, street, buildingNumber, postCode, city, manager, salesman, administrator) {
     const data = {
